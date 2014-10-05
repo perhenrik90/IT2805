5c60192a66919e40fb0e6898015bd9363eacdbf1
Per-Henrik E Kvalnes <perhenrik.kvalnes@gmail.com>
2014-10-05 16:01:32 +0200

    syntax headlighter prioriterer nå tekststrenger

--- syntax/python.js
+++ syntax/python.js
@@ -17,12 +17,40 @@ function applyHeadlight(preElement)
     c = c.replace(/(if)/g, "<key>$1</key>");
     c = c.replace(/(else)/g, "<key>$1</key>");
     c = c.replace(/(while)/g, "<key>$1</key>");
+    c = c.replace(/(and)/g, "<key>$1</key>");
+    c = c.replace(/(del)/g, "<key>$1</key>");
+    c = c.replace(/(from)/g, "<key>$1</key>");
+    c = c.replace(/(not)/g, "<key>$1</key>");
+    c = c.replace(/(as)/g, "<key>$1</key>");
+    c = c.replace(/(elif)/g, "<key>$1</key>");
+    c = c.replace(/(global)/g, "<key>$1</key>");
+    c = c.replace(/(or)/g, "<key>$1</key>");
+    c = c.replace(/(width)/g, "<key>$1</key>");
+    c = c.replace(/(assert)/g, "<key>$1</key>");
+    c = c.replace(/(pass)/g, "<key>$1</key>");
+    c = c.replace(/(yield)/g, "<key>$1</key>");
+    c = c.replace(/(break)/g, "<key>$1</key>");
+    c = c.replace(/(except)/g, "<key>$1</key>");
+    c = c.replace(/(import)/g, "<key>$1</key>");
+    c = c.replace(/(exec)/g, "<key>$1</key>");
+    c = c.replace(/(in)/g, "<key>$1</key>");
+    c = c.replace(/(raise)/g, "<key>$1</key>");
+    c = c.replace(/(continue)/g, "<key>$1</key>");
+    c = c.replace(/(finaly)/g, "<key>$1</key>");
+    c = c.replace(/(is)/g, "<key>$1</key>");
+    c = c.replace(/(for)/g, "<key>$1</key>");
+    c = c.replace(/(lambda)/g, "<key>$1</key>");
+    c = c.replace(/(try)/g, "<key>$1</key>");
 
     // headlight tall
     c = c.replace(/(\d+)/g, "<number>$1</number>");
 
     // headlight strings
     c = c.replace(/(".*")/g, "<string>$1</string>");
+
+
+    // headlight comments
+    c = c.replace(/(#.*\n)/g, "<comment>$1</comment>");
     preElement.innerHTML = c;
 }
 
