8a8a6bac8e0df9073f24fb567802c128fd96131c
Per-Henrik E Kvalnes <perhenrik.kvalnes@gmail.com>
2014-10-09 18:58:42 +0200

    added more string syntax

--- syntax/python.js
+++ syntax/python.js
@@ -7,7 +7,7 @@
 function applyHeadlight()
 {
     elements = document.getElementsByClassName("syntax:python");
-    alert(elements.length);
+
     for(i = 0; i < elements.length; i ++)
     {
 	element = elements[i];
@@ -22,11 +22,11 @@ function applyHeadlightForElement(preElement)
     c = preElement.innerHTML;
     
     // headlight keywords
-    c = c.replace(/(def)/g, "<key>$1</key>");
-    c = c.replace(/(print)/g, "<key>$1</key>");
-    c = c.replace(/(return)/g, "<key>$1</key>");
-    c = c.replace(/(class)/g, "<key>$1</key>");
-    c = c.replace(/(if)/g, "<key>$1</key>");
+    c = c.replace(/ (def) /g, "<key> $1 </key>");
+    c = c.replace(/ (print) /g, "<key> $1 </key>");
+    c = c.replace(/ (return) /g, "<key> $1 </key>");
+    c = c.replace(/ (class) /g, "<key> $1 </key>");
+    c = c.replace(/ (if) /g, "<key> $1 </key>");
     c = c.replace(/(else)/g, "<key>$1</key>");
     c = c.replace(/(while)/g, "<key>$1</key>");
     c = c.replace(/(and)/g, "<key>$1</key>");
@@ -43,22 +43,23 @@ function applyHeadlightForElement(preElement)
     c = c.replace(/(yield)/g, "<key>$1</key>");
     c = c.replace(/(break)/g, "<key>$1</key>");
     c = c.replace(/(except)/g, "<key>$1</key>");
-    c = c.replace(/(import)/g, "<key>$1</key>");
+    c = c.replace(/ (import) /g, "<key> $1 </key>");
     c = c.replace(/(exec)/g, "<key>$1</key>");
-    c = c.replace(/(in)/g, "<key>$1</key>");
+    c = c.replace(/ (in) /g, "<key>$1</key>");
     c = c.replace(/(raise)/g, "<key>$1</key>");
     c = c.replace(/(continue)/g, "<key>$1</key>");
     c = c.replace(/(finaly)/g, "<key>$1</key>");
-    c = c.replace(/(is)/g, "<key>$1</key>");
-    c = c.replace(/(for)/g, "<key>$1</key>");
+    c = c.replace(/ (is) /g, "<key> $1 </key>");
+    c = c.replace(/ (for) /g, "<key> $1 </key>");
     c = c.replace(/(lambda)/g, "<key>$1</key>");
-    c = c.replace(/(try)/g, "<key>$1</key>");
+    c = c.replace(/ (try) /g, "<key> $1 </key>");
 
     // headlight tall
     c = c.replace(/(\d+)/g, "<number>$1</number>");
 
     // headlight strings
     c = c.replace(/(".*")/g, "<string>$1</string>");
+    c = c.replace(/('.*')/g, "<string>$1</string>");
 
 
     // headlight comments
