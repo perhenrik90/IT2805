782c8434cf29a6526c46ae705b34d7501744adb2
Per-Henrik E Kvalnes <perhenrik.kvalnes@gmail.com>
2014-10-26 12:00:56 +0100

    lagt til et fungerende trofeelager

--- trophies/index.js
+++ trophies/index.js
@@ -6,17 +6,26 @@
 
 function initSite()
 {
-    tl = document.getElementById("tropylist");
+    tl = document.getElementById("trophylist");
     
     ts = connectTrophyStorage();
     trophies = ts.getTrophies();
+    
+    if(trophies.length == 0)
+    {
+	tl.innerHTML = "Du har ingen trofeer ennå!";
+	return;
+    }
 
     table = document.createElement("table");
+    table.className = "trophyTable";
+
     tl.appendChild(table);
     for(i in trophies)
     {
 	value = trophies[i]
 	tr = document.createElement("tr");
+	table.appendChild(tr);
 
 	td1 = document.createElement("td");
 	tr.appendChild(td1);
@@ -27,4 +36,4 @@ function initSite()
 	td2.innerHTML = value;
     }
 }
-window.addEventListener("load", initSide);
+window.addEventListener("load", initSite);
