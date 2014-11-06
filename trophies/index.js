5c370d6e693fa484a60be41767fd0712e9be472b
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2014-11-06 15:51:14 +0100

    lagt til en mapping fra verdi til trofe bilde

--- trophies/index.js
+++ trophies/index.js
@@ -33,7 +33,33 @@ function initSite()
 
 	td2 = document.createElement("td");
 	tr.appendChild(td2);
-	td2.innerHTML = value;
+	img = valueToTrophy(value);
+	td2.appendChild(img);
     }
 }
 window.addEventListener("load", initSite);
+
+
+/***********************************************
+ * Tar en verdi (1,2,3) og gir et pokalbilde
+ ***********************************************/
+function valueToTrophy(value)
+{
+    var img = new Image();
+    img.className = "smallTrophy";
+    var path = "../img/"
+    if(value == 1)
+    {
+	img.src = path+"pokal_bronze.png";
+    }
+    else if(value == 2)
+    {
+	img.src = path+"pokal_silver.png";
+    }
+    else if(value == 3)
+    {
+	img.src = path+"pokal_gold.png";
+    }
+    
+    return img;
+}
