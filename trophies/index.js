ed5df981c4c0512ef8d579dba0c735a3910598cc
Per-Henrik E Kvalnes <perhenrik.kvalnes@gmail.com>
2014-11-15 17:49:02 +0100

    lagt til inline trofeer

--- trophies/index.js
+++ trophies/index.js
@@ -17,24 +17,23 @@ function initSite()
 	return;
     }
 
-    table = document.createElement("table");
-    table.className = "trophyTable";
-
-    tl.appendChild(table);
     for(i in trophies)
     {
+	div = document.createElement("div");
+	div.className = "trophyCase";
+	tl.appendChild(div);
+
+	// add trophy image
 	value = trophies[i]
-	tr = document.createElement("tr");
-	table.appendChild(tr);
-
-	td1 = document.createElement("td");
-	tr.appendChild(td1);
-	td1.innerHTML = i;
-
-	td2 = document.createElement("td");
-	tr.appendChild(td2);
 	img = valueToTrophy(value);
-	td2.appendChild(img);
+	div.appendChild(img);
+
+	// add trophy text/type
+	p = document.createElement("p");
+	div.appendChild(p);
+	p.className = "trophyText";
+	p.innerHTML = i;
+	
     }
 }
 window.addEventListener("load", initSite);
